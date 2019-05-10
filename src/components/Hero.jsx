60817fc7b2fc4cbe40b59760470bdf5319599b7d
Sabine Robart <sabine.robart@protonmail.com>
2019-05-10 17:15:20 +0200

    [Upd] Hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,67 +1,135 @@
 import React, { Component } from "react";
-import logo from "../logo.svg";
 import "../style/Hero.scss";
+import reactLogo from "../pictures/icons/logo.svg";
+import { Col } from "reactstrap";
 
 class Hero extends Component {
   render() {
     const { basics } = this.props.resumeObj;
-
     return (
-      <div className="hero">
-        <div className="nameBg border">
-          <h1 className="hero__header">{basics.name}</h1>
-        </div>
-        <h4 className="hero__subheader">
-          {basics.location.city}{" "}
-          <span role="img" aria-label="Sun">
-            ☀️
-          </span>
-        </h4>
+      <Col lg="4" className="hero px-5">
+        <div className="business-card">
+          <div className="flip hvr-outline-in">
+            <div className="front d-flex align-items-center">
+              <Col xs="4">
+                <img
+                  src={require("../pictures/PhotoCV-b&w.jpeg")}
+                  height="100vh"
+                  alt="profile"
+                />
+              </Col>
+              <Col xs="8">
+                <h1>{basics.firstName}</h1>
+                <h1>{basics.lastName}</h1>
+                <div className="rotate">
+                  <i className="fas fa-redo-alt" />
+                </div>
+              </Col>
+            </div>
 
-        <p>{basics.summary}</p>
+            <div className="back">
+              <h2>Web Developer</h2>
+              <h3>Front-end | Back-end</h3>
+              <div className="line" />
+              <div className="social">
+                <a href={basics.social.linkedin}>
+                  <i className="fab fa-linkedin-in" />
+                </a>
+                <a href={basics.social.github}>
+                  <i className="fab fa-github" />
+                </a>
+                <a href={basics.social.twitter}>
+                  <i className="fab fa-twitter" />
+                </a>
+                <a href={basics.social.codepen}>
+                  <i className="fab fa-codepen" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hero__subheader" />
+
+        <div className="hero__summary">
+          <p>{basics.summary}</p>
+          <h4>
+            {basics.specialties.map((techno, i) => {
+              return (
+                <span key={i} className="mr-3 font-italic">
+                  #{techno}
+                </span>
+              );
+            })}
+          </h4>
+        </div>
         {basics.pdf ? (
-          <p className="hero__text">
-            <a href={basics.pdf}>PDF version</a> of the resume.
-          </p>
+          <a
+            href={basics.pdf}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="links"
+          >
+            <div>
+              <img
+                src={require("../pictures/icons/pdf.png")}
+                height="20px"
+                alt="pdf"
+              />
+              <span>Version PDF</span>
+            </div>
+          </a>
         ) : (
           ""
         )}
-        <p className="hero__text">
-          <a
-            href={`mailto:${basics.email}?subject=Resume%20Inquiry`}
-            style={{ textDecoration: "none" }}
-          >
-            e-mail
-          </a>
+        <a
+          href={basics.social.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="links"
+        >
+          <div>
+            <img
+              src={require("../pictures/icons/linkedin.png")}
+              height="20px"
+              alt="linkedin"
+            />
+            <span>Voir mon profil Linkedin</span>
+          </div>
+        </a>
+        <a
+          href={`mailto:${basics.social.email}?subject=Resume`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="links"
+        >
+          <div>
+            <img
+              src={require("../pictures/icons/mail.png")}
+              height="20px"
+              alt="email"
+            />
+            <span>Me contacter par e-mail</span>
+          </div>
+        </a>
+
+        <p className="made-with">
+          Powered by
+          <img
+            src={require("../pictures/icons/heart.png")}
+            alt="love"
+            className="hero__heart"
+          />
+          and
+          <img src={reactLogo} className="hero__react-logo" alt="react" />
         </p>
-
-        <p className="hero__text">
-          <a
-            href="https://linkedin.com/in/iamskok"
-            style={{ textDecoration: "none" }}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {basics.website}
-          </a>
-        </p>
-
-        <div className="hero__link-container">
-          <a
-            href="https://github.com/iamskok/react-resume"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hero__link"
-          >
-            made with
-            <img src={logo} className="hero__logo" alt="Made With React Logo" />
-          </a>
-        </div>
-
         <span role="img" aria-label="Arrow down" className="hero__arrow">
-          ⬇️
+          <img
+            src={require("../pictures/icons/arrow-down.png")}
+            alt="arrow down"
+            height="25px"
+          />
         </span>
-      </div>
+      </Col>
     );
   }
 }
