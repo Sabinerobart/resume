b5a2c9ed1d2a302220e776ef599abc8ac17c5ed4
Sabine Robart <sabine.robart@protonmail.com>
2019-05-10 17:18:11 +0200

    [Upd] skills (layout + info).

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
 
 class Skill extends Component {
   render() {
     return (
-      <li className="skill">
-        {this.props.details}
-      </li>
+      <Row className="skill-row">
+        <Col xs="3" className="skill-number align-items-center">
+          <p className="m-0">{this.props.index + 1}</p>
+        </Col>
+        <Col xs="8" className="skill d-flex align-items-center mx-auto">
+          <p className="m-0">{this.props.details}</p>
+        </Col>
+      </Row>
     );
   }
 }
--- src/components/SkillsList.jsx
+++ src/components/SkillsList.jsx
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import Skill from './Skill';
+import React, { Component } from "react";
+import Skill from "./Skill";
+import "../style/SkillsList.scss";
+import "../App.scss";
+import { Col } from "reactstrap";
+// import Footer from "./Footer";
 
 class SkillsList extends Component {
   render() {
     const { skills } = this.props.resumeObj;
-  
+
     return (
-      <div
-        className="skills-list__container"
-        style={{display: 'flex'}}
-      >
-        <div className="skills-list__modal">
-          <h1 className="skills-list__header">Skill Set</h1>
-          <br />
-          <ul className="skills-list">
-            {
-              skills.keywords
-                .map((key, index) => <Skill key={key} details={skills.keywords[index]}/>)
-            }
-          </ul>
+      <Col lg="7" className="skills-list__container">
+        <h1 className="title skills-list__header">
+          Compétences <span>+</span>
+        </h1>
+        <div className="separation" />
+        <div className="skills-list__modal row text-center text-md-left">
+          <Col xs="12" md="6" className="skills-list my-4">
+            <h3>Compétences techniques</h3>
+            {skills.keywords.map((key, i) => (
+              <Skill key={i} details={skills.keywords[i]} index={i} />
+            ))}
+          </Col>
+          <Col xs="12" md="6" className="skills-list my-4">
+            <h3>Je suis aussi</h3>
+            {skills.softSkills.map((key, i) => (
+              <Skill key={i} details={skills.softSkills[i]} index={i} />
+            ))}
+          </Col>
         </div>
-      </div>
+        {/* <Footer /> */}
+      </Col>
     );
   }
 }
